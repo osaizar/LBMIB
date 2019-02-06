3be183038c898f3c209dff59ba3fef88ad9ffaf7
osaizar <oierlas@gmail.com>
2019-02-06 12:41:08 +0100

    lbmib_cli - hash

--- zerbitzaria/lbmib_cli.py
+++ zerbitzaria/lbmib_cli.py
@@ -1,5 +1,6 @@
 from models import User
 from database import db_session
+from werkzeug.security import generate_password_hash
 
 import sys
 
@@ -20,7 +21,7 @@ def main():
         if len(sys.argv) != 4:
             print("[-] new-user <username> <password>")
         else:
-            user = User(sys.argv[2], sys.argv[3])
+            user = User(sys.argv[2], generate_password_hash(sys.argv[3]))
             correct, data = add(user)
             if correct:
                 print("[+] "+data.username+" gehitu da")
